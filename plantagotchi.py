ccf09ef23bec4071a2845eeb512bcf66a9ebc58c
bazonkey <144395686+bazonkey@users.noreply.github.com>
2026-04-26 17:11:04 -0400

    OPC UA write

--- plantagotchi.py
+++ plantagotchi.py
@@ -124,7 +124,7 @@ class Tama(pygame.sprite.Sprite):
     # OPCUA DATA
     # ---------------------------
     def opc_update(self, water, nutrients, light, age):
-        self.thirst = ((2000+water) / WATER_MAX) * 100
+        self.thirst = ((2000-water) / WATER_MAX) * 100
         self.vitality = max(0, nutrients / FOOD_MAX * 100)
         self.mood = max(0, light / LIGHT_MAX * 100)
         self.age = int(age)
